1ca7a87fe24921d388cc90c198fb12384b909dd3
Bolb2019 <zachnoahstein@gmail.com>
2026-07-16 12:16:37 -0400

    better move code

--- Player/character_body_2d.gd
+++ Player/character_body_2d.gd
@@ -91,12 +91,13 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
-	if velocity.x >= 0:
-		velocity.x -= FRICTION 
+	if velocity.x > FRICTION:
+		velocity.x -= FRICTION
+	elif velocity.x < -FRICTION:
+		velocity.x += FRICTION
 	else:
-		velocity.x += FRICTION 
-	if abs(velocity.x) <= 10 and abs(velocity.x) >= 0.0:
 		velocity.x = 0
+
 
 	move_and_slide()
 	
